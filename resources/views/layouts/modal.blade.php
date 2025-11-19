07c6cd94c2008133787daebf4183734aca2181cb
danial <muhddanialhakim0224@gmail.com>
2025-11-19 11:30:10 +0800

    Commit layouts modal

---
+++ resources/views/layouts/modal.blade.php
@@ -0,0 +1,27 @@
+<div class="modal fade" id="daftarKursusModal" tabindex="-1" aria-labelledby="daftarKursusLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="daftarKursusLabel">Daftar Kursus</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+        <div class="modal-body">
+            <form id="formDaftarKursus">
+                <div class="mb-3">
+                    <label for="namaKursus" class="form-label">Nama Kursus</label>
+                    <input type="text" class="form-control" id="namaKursus" name="namaKursus" placeholder="Contoh: Kursus Kepimpinan">
+                </div>
+            <div class="mb-3">
+                <label for="tarikh" class="form-label">Tarikh</label>
+                <input type="date" class="form-control" id="tarikh" name="tarikh">
+            </div>
+            <div class="mb-3">
+                <label for="lokasi" class="form-label">Lokasi</label>
+                <input type="text" class="form-control" id="lokasi" name="lokasi" placeholder="Contoh: Dewan Seminar A">
+            </div>
+            <button type="submit" class="btn btn-primary">Simpan</button>
+            </form>
+        </div>
+        </div>
+    </div>
+</div>

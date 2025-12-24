336f77cefd8175a211e04665d9b5188cd612f4bc
danial <muhddanialhakim0224@gmail.com>
2025-12-24 17:32:46 +0800

    Edit Icon and Menu

--- resources/views/partials/sidebarAdmin.blade.php
+++ resources/views/partials/sidebarAdmin.blade.php
@@ -4,14 +4,14 @@
             <img class="kedah-img" src="{{ asset("assets/img/cropped-kedah-baru.png") }}" alt="logoKedah" style="max-width: 50px; margin-right:30px">
         </div>
         <div class="sidebar-header">
-            <h2 class="text-primary pt-sans-bold" style="font-size: 26px;">Admin Panel</h2>
+            <h2 class="gabarito-regular">Admin Panel</h2>
             <i class="bi bi-list sidebar-toggle" style="margin-bottom: 5px" id="sidebarToggle"></i>
         </div>
         <ul class="sidebar-menu">
             <li class="{{ request()->routeIs('adminView') ? 'active' : '' }}" title="Pending Users">
                 <a href="{{ route('adminView') }}">
-                    <i class="fas fa-user-clock"></i>
-                    <span class="menu-text">Pending Users</span>
+                    <i class="bi bi-person-fill-add"></i>
+                    <span class="menu-text">Daftar Pengguna</span>
                 </a>
             </li>
             <li class="{{ request()->routeIs('view') ? 'active' : '' }}" title="All Users">
@@ -21,8 +21,15 @@
                     <span class="menu-text">All Users</span>
                 </a>
             </li>
-            <li>
-                <a href="#" title="Settings">
+            <li class="" title="Jabatan">
+                <a href="#" title="Jabatan">
+                    <i class="fa fa-building"></i>
+                    <span class="material-symbols-outlined"></span>
+                    <span class="menu-text">Jabatan</span>
+                </a>
+            </li>
+            <li class="{{ request()->routeIs('admin.setting') ? 'active': '' }}" title="Settings">
+                <a href="{{ route('admin.setting') }}" title="Settings">
                     <i class="fas fa-cogs"></i>
                     <span class="menu-text">Settings</span>
                 </a>
